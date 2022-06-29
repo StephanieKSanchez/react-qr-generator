0712f82af3b280e919e50a27aa8fac3421c9c168
Stephanie Sanchez <stephk006@gmail.com>
2022-06-29 18:15:47 -0400

    qr code can be generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import QRCode from "qrcode";
+import { useState } from "react";
+
 function App() {
+  const [url, setUrl] = useState("");
+  const [qrCode, setQrCode] = useState("");
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, (err, url) => {
+      if (err) return console.error(err);
+
+      console.log(url);
+      setQrCode(url);
+    });
+  };
   return (
     <div className="app">
       <h1>QR Code Generator</h1>
-      <input type="text" placeholder="ex. https;//google.com" />
-      <button>Generate</button>
+      <input
+        type="text"
+        placeholder="ex. https;//google.com"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={GenerateQRCode}>Generate</button>
+      <img src={qrCode} />
     </div>
   );
 }
