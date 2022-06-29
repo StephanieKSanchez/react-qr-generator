4b77d85f37296d3cc52465eda8e5a2b453372c4f
Stephanie Sanchez <stephk006@gmail.com>
2022-06-29 16:51:03 -0400

    created input field & button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="app">
-      <h1>Hello, World!</h1>
+      <h1>QR Code Generator</h1>
+      <input type="text" placeholder="ex. https;//google.com" />
+      <button>Generate</button>
     </div>
   );
 }
